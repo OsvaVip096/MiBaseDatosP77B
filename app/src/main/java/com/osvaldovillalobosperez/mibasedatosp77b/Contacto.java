f586f0f48328572efbcdb53d1fd7ec703b9cc329
Osvaldo Villalobos Pérez <s15120062@alumnos.itsur.edu.mx>
2019-10-11 23:08:08 -0500

    Comit inicial con la estructura base vista en la clase de Móvil I.

---
+++ app/src/main/java/com/osvaldovillalobosperez/mibasedatosp77b/Contacto.java
@@ -0,0 +1,47 @@
+package com.osvaldovillalobosperez.mibasedatosp77b;
+
+public class Contacto {
+    int id;
+    String usuario;
+    String email;
+    String tel;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public Contacto(int id, String usuario, String email, String tel) {
+        this.id = id;
+        this.usuario = usuario;
+        this.email = email;
+        this.tel = tel;
+    }
+}
---
+++ app/src/main/java/com/osvaldovillalobosperez/mibasedatosp77b/MainActivity.java
@@ -0,0 +1,60 @@
+package com.osvaldovillalobosperez.mibasedatosp77b;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.database.Cursor;
+import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView lv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        DAOContacto dao = new DAOContacto(this);
+
+        dao.insert(
+                new Contacto(
+                        0,
+                        "Osva",
+                        "s15120062@alumnos.itsur.edu.mx",
+                        "4451597776"
+                )
+        );
+
+        dao.insert(
+                new Contacto(
+                        0,
+                        "Jesús",
+                        "s29823933@alumnos.itsur.edu.mx",
+                        "4452880110"
+                )
+        );
+
+        for (Contacto c : dao.getAll()) {
+            Toast.makeText(this, c.getUsuario(), Toast.LENGTH_SHORT).show();
+        }
+
+        Cursor c = dao.getAllCursor();
+
+        lv = findViewById(R.id.Lv);
+
+        SimpleCursorAdapter adp = new SimpleCursorAdapter(
+                this,
+                android.R.layout.simple_expandable_list_item_2,
+                c,
+                new String[]{"usuario", "email"},
+                new int[]{android.R.id.text1, android.R.id.text2},
+                SimpleCursorAdapter.IGNORE_ITEM_VIEW_TYPE
+        );
+
+        lv.setAdapter(adp);
+    }
+}
